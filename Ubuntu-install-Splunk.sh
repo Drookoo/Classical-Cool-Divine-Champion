25956c549365ce8de6b88d63610ca3e7ef54f1ab
Drew Ku <andrewku123@gmail.com>
2019-10-11 18:23:51 -0400

    splunk edits

--- Ubuntu-install-Splunk.sh
+++ Ubuntu-install-Splunk.sh
@@ -4,18 +4,19 @@
 # Splunk UFW .deb: https://wfs.pace.edu/browse/anPMcWByl0eNoRn01ReEcw/
 
 # Splunk Enterprise
-sudo apt-get install python3-pip -y 
-sudo pip3 install gdown 
-mkdir /home/ccdc/temp 
-cd /home/ccdc/temp
-gdown https://drive.google.com/uc?id=1uVzPevLbm_293dWPK9f1n2BU0TlP9hmS
-sudo dpkg -i Ubuntu-splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
+# sudo apt-get install python3-pip -y 
+# sudo pip3 install gdown 
+# mkdir /home/ccdc/temp 
+# cd /home/ccdc/temp
+# gdown https://drive.google.com/uc?id=1uVzPevLbm_293dWPK9f1n2BU0TlP9hmS
+# sudo dpkg -i Ubuntu-splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
 
 # Splunk UF 
-sudo pip3 install gdown
+# sudo pip3 install gdown
 #mkdir /home/ccdc/temp
-cd /home/ccdc/temp
-gdown https://drive.google.com/uc?id=1ss91UuvAd8o73K2Fqtjj0Hzvdx14RdFk
+# cd /home/ccdc/temp
+#gdown https://drive.google.com/uc?id=1ss91UuvAd8o73K2Fqtjj0Hzvdx14RdFk
+wget https://wfs.pace.edu/browse/anPMcWByl0eNoRn01ReEcw/
 sudo dpkg -i Ubuntu-splunkforwarder-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
 
 # Configure Splunk Enterprise 
