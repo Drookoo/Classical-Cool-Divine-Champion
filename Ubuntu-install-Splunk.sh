0fb33adefe107facae7f59e20f2cefd4af10ee04
Drew Ku <andrewku123@gmail.com>
2019-10-11 18:46:39 -0400

    hello

--- Ubuntu-install-Splunk.sh
+++ Ubuntu-install-Splunk.sh
@@ -16,7 +16,7 @@
 #mkdir /home/ccdc/temp
 # cd /home/ccdc/temp
 #gdown https://drive.google.com/uc?id=1ss91UuvAd8o73K2Fqtjj0Hzvdx14RdFk
-wget https://wfs.pace.edu/browse/anPMcWByl0eNoRn01ReEcw/
+wget https://wfs.pace.edu/browse/uIYoP2juAUmr7jYNP5nN6Q/
 sudo dpkg -i Ubuntu-splunkforwarder-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
 
 # Configure Splunk Enterprise 
