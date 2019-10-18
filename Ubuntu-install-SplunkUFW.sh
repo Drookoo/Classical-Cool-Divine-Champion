57f3e0212e8066ccb7c311dea10da2102fc1e8bb
Drew Ku <andrewku123@gmail.com>
2019-10-18 00:09:13 -0400

    enterprise

--- Ubuntu-install-SplunkUFW.sh
+++ Ubuntu-install-SplunkUFW.sh
@@ -14,20 +14,22 @@
 # sudo dpkg -i Ubuntu-splunk-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
 
 # Splunk UF 
-# sudo pip3 install gdown
-#mkdir /home/ccdc/temp
-# cd /home/ccdc/temp
-#gdown https://drive.google.com/uc?id=1ss91UuvAd8o73K2Fqtjj0Hzvdx14RdFk
-#wget https://wfs.pace.edu/browse/uIYoP2juAUmr7jYNP5nN6Q/
-#sudo dpkg -i Ubuntu-splunkforwarder-7.3.1.1-7651b7244cf2-linux-2.6-amd64.deb
+# Readme: https://docs.splunk.com/Documentation/Forwarder/7.3.2/Forwarder/Configuretheuniversalforwarder
+wget -O splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.2&product=universalforwarder&filename=splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb&wget=true'
+sudo dpkg -i splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb
+cd /opt/splunkforwarder/bin 
+sudo ./splunk enable boot-start
+sudo ./splunk add forward-server 10.0.2.12:9997
+sudo ./splunk add monitor /var/log
+sudo ./splunk restart
+
 
 # Configure Splunk Enterprise 
 #cd /opt/splunk/bin/
 #sudo ./splunk enable boot-start
 #follow prompts, do things 
 #sudo service splunk start
-
-
-wget -O splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.2&product=universalforwarder&filename=splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb&wget=true'
-
 #configure splunk enterprise to recieve data on port 9997
+
+
+
---
+++ Ubuntu-install-splunkEnt.sh
@@ -0,0 +1,18 @@
+#/bin/bash 
+
+# Splunk Enterprise .deb: https://wfs.pace.edu/browse/LolUnH_KB0CdLHap3f3gHw/
+
+# Splunk Enterprise
+wget -O splunk-7.3.2-c60db69f8e32-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.3.2&product=splunk&filename=splunk-7.3.2-c60db69f8e32-linux-2.6-amd64.deb&wget=true'
+
+sudo dpkg -i splunk-7.3.2-c60db69f8e32-linux-2.6-amd64.deb
+
+# Configure Splunk Enterprise 
+cd /opt/splunk/bin/
+#sudo ./splunk enable boot-start
+#follow prompts, do things 
+#sudo service splunk start
+#configure splunk enterprise to recieve data on port 9997
+
+
+
