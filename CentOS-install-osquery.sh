f79b31d9713e4c1b9fb374f4c11f3ec7779d9bde
Drew Ku <andrewku123@gmail.com>
2019-11-01 16:17:18 -0400

    changes

--- CentOS-install-osquery.sh
+++ CentOS-install-osquery.sh
@@ -4,3 +4,4 @@ sudo curl -L https://pkg.osquery.io/rpm/GPG | sudo tee /etc/pki/rpm-gpg/RPM-GPG-
 sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
 sudo yum-config-manager --enable osquery-s3-rpm
 sudo yum install osquery -y
+
