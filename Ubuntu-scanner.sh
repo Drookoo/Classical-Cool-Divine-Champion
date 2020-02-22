df6f23fdda999da55c2e16064e122a9c22e5ecf1
Drew Ku <andrewku123@gmail.com>
2020-02-22 13:36:20 -0500

    addition of fast scan to nmap script

--- Ubuntu-scanner.sh
+++ Ubuntu-scanner.sh
@@ -6,16 +6,19 @@ echo "================================"
 echo "Ubuntu Scanner Menu" 
 echo "================================"
 echo "Enter 1 to install prereqs 1:"
-echo "Enter 2 for fast scan 2:"
-echo "Enter 3 for slow scan 3:" 
+echo "Enter 2 for NMAP ping scan 2:"
+echo "Enter 3 for NMAP slow scan + fast exec -A -T4 scan 3:" 
 echo "Enter q to exit the menu q:"
 echo -e "\n"
 echo -e "Enter your selection \c"
 read answer
 case "$answer" in 
 1) sudo apt-get install nmap xsltproc -y
+	sudo mkdir /tmp/nmap_scans
 	echo "Prereqs installed" ;;
-2) who ;;
+2) echo -e "Enter the host or range of ips for target \c"
+    read "$host" 
+	sudo nmap -sn $host -oX /tmp/nmap_scans/fast_scan/nmap_$date.xml
 3) cal ;;
 q) exit ;; 
 esac 
