51be1531ac627d017cbffb779f9d5d61cd18479b
Drew Ku <andrewku123@gmail.com>
2020-02-22 14:20:17 -0500

    move

--- Ubuntu-scanner.sh
+++ Ubuntu-scanner.sh
@@ -18,7 +18,7 @@ case "$answer" in
 	echo "Prereqs installed" ;;
 2) echo -e "Enter the host or range of ips for target \c"
     read "$host" 
-	sudo nmap -sn $host -oX /tmp/nmap_scans/fast_scan/nmap_$date.xml
+	sudo nmap -sn $host -oX /tmp/nmap_scans/fast_scan/nmap_$date.xml;;
 3) cal ;;
 q) exit ;; 
 esac 
